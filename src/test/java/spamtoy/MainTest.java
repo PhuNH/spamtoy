fab56fb5af11011e2c08f541a56591dee77abf2c
PhuNH <1079742+PhuNH@users.noreply.github.com>
2023-11-10 00:12:05 +0100

    Initial commit

---
+++ src/test/java/spamtoy/MainTest.java
@@ -0,0 +1,31 @@
+package spamtoy;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testOut() {
+        Main.main(new String[]{""});
+        assertEquals("Hello world!\n", outContent.toString());
+    }
+}
